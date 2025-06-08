971510e041e0bcd59cd6b2fd8aa032a93bd68ed0
jasonSuiyt <565784544@qq.com>
2025-06-08 22:57:54 +0800

    refactor: 移除未使用的tauri插件和依赖项
    清理项目中未使用的tauri插件(fs, http, notification, shell)及相关依赖
    更新权限配置和Cargo.lock文件以反映这些变更

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -19,10 +19,6 @@ fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_notification::init())
-        .plugin(tauri_plugin_http::init())
-        .plugin(tauri_plugin_fs::init())
         .setup(|_app| {
             db::init();
             APP.lock().unwrap().insert(
